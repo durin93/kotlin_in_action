01dad8dc410915d754ffc76d56e61a5c9bc67a5d
gram.eu(이승철) <gram.eu@kakaomobility.com>
2018-12-28 18:38:24 +0900

    파라미터 이름뒤에 그 파라미터의 타입을쓴다

--- src/Person.kt
+++ src/Person.kt
@@ -4,4 +4,10 @@ fun main(args: Array<String>) {
     val persons = listOf(Person("영희"),Person("철수",29))
     val oldest = persons.maxBy { it.age ?: 0}
     println("나이가 가장 많은 사람: $oldest")
+
+    println(max(1,2))
+}
+
+fun max(a: Int, b: Int): Int{
+    return if(a>b) a else b
 }
