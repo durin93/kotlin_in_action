17c5dd0f224db480a0c6608e6f3c352277a73433
gram.eu(이승철) <gram.eu@kakaomobility.com>
2018-12-29 16:48:24 +0900

    when enum

---
+++ src/Person2.kt
@@ -0,0 +1,52 @@
+import kotlin.math.ln
+
+data class Person2(val name : String, val age : Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person2("영희"),Person2("철수",29))
+    val oldest = persons.maxBy { it.age ?: 0}
+    println("나이가 가장 많은 사람: ${oldest}")
+    println(max(1,2))
+
+    val name = if(args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name")
+
+    println(Color.BLUE.rgb())
+    println(mix(Color.YELLOW,Color.RED))
+    println(mix(Color.RED,Color.YELLOW))
+    println(mixOptimized(Color.RED,Color.RED))
+
+
+}
+/*
+
+fun max(a: Int, b: Int): Int{
+    return if(a>b) a else b
+}
+*/
+
+fun max(a: Int, b: Int): Int = if(a>b) a else b
+
+fun getMneomnic(color: Color) =
+    when (color){
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        else  -> throw Exception("else")
+    }
+
+fun mix(c1: Color, c2: Color) =
+    when(setOf(c1,c2)){
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.INDIGO
+        else -> Color.INDIGO
+    }
+
+fun mixOptimized(c1: Color, c2: Color): Color {
+    val color = when {
+        (c1 == Color.RED && c2 == Color.YELLOW) ||
+            (c1 == Color.YELLOW && c2 == Color.RED) -> Color.ORANGE
+        else -> Color.INDIGO
+    }
+    return color
+}
